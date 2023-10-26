56468a635895d475cda7ad98d60770554e3345a6
YagoHFA <yagof2421@gmail.com>
2023-10-26 00:20:04 -0300

    Consertando erros javadoc

--- src/Models/Interfaces/Divisao.java
+++ src/Models/Interfaces/Divisao.java
@@ -10,9 +10,9 @@ public interface Divisao {
      * @param a uma do tipo variavel inteira para compor a logica de divisao a ser uilizada
      * @param b uma do tipo variavel inteira para compor a logica de divisao a ser uilizada
      *
-     * @return {@code int} o resultado da divisao entre variavél a e b, como sendo do tipo inteiro
+     * @return {@code int} o resultado da divisao entre variavel a e b, como sendo do tipo inteiro
      *
-     * @throws ArithmeticException lanca excecao, para que divisão por zero possa ser tratada
+     * @throws ArithmeticException lanca excecao, para que divisao por zero possa ser tratada
      */
     public int divisao(int a, int b) throws  ArithmeticException;
 }
